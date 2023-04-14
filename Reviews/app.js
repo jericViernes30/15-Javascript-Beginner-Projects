c388c4e7961d2888b1efe7dfb590d989100191cb
Jeric Viernes <jericviernes06@gmail.com>
2023-04-14 21:16:00 +0800

    added DOMContentLoaded event listener

--- Reviews/app.js
+++ Reviews/app.js
@@ -3,14 +3,72 @@ const reviews = [
         id:1,
         name:"Jeric James Viernes",
         job:"Full stack developer",
-        img:"pic-2.jpg",
-        text:"Hello, my name is Jeric James Viernes. I'm 20 years old from Cabuyao Laguna. I'm currently at 2nd year college taking Bachelor of Science in Information Technology. I specialized in programming language such as Javascript and Python."
+        img:"pic-1.jpg",
+        text:"Hello, my name is Jeric James Viernes. I'm 20 years old from Cabuyao Laguna. I'm currently at 2nd year college taking Bachelor of Science in Information Technology. I specialized in programming language such as Javascript and Python.",
     },
     {
         id:2,
         name:"ChatGPT",
         job:"AI Model",
         img:"pic-3.jpg",
-        text:"Hello, my name is ChatGPT. I am a language model designed to assist and communicate with humans. My purpose is to provide helpful and informative responses to any questions or inquiries you may have."
+        text:"Hello, my name is ChatGPT. I am a language model designed to assist and communicate with humans. My purpose is to provide helpful and informative responses to any questions or inquiries you may have.",
+    },
+    {
+        id:3,
+        name:"David Shane",
+        job:"UX Designer",
+        img:"pic-2.jpg",
+        text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor fugit esse voluptatum dignissimos aperiam suscipit, rerum accusamus cupiditate non hic.",
     }
+
 ]
+
+// select items
+const img = document.getElementById('person-img')
+const author = document.getElementById('author')
+const job = document.getElementById('job')
+const info = document.getElementById('info')
+
+const prevBtn = document.querySelector('.prev-btn')
+const nextBtn = document.querySelector('.next-btn')
+const surpBtn = document.querySelector('.surprise-btn')
+
+let currentItem = 0
+
+// set starting item
+
+const item = reviews[currentItem] //assigning to item for easier access to property like shown below
+// load initial item\
+window.addEventListener('DOMContentLoaded',() => {
+    showPerson()
+})
+
+function showPerson() {
+    img.src = item.img
+    authorStyle()
+    jobStyle()
+    infoStyle()
+  
+}
+
+function jobStyle() {
+    job.textContent = item.job 
+    job.style.fontSize = "10px"
+    job.style.color = "rgb(69, 189, 229)"
+    job.style.textTransform = "uppercase"
+    job.style.fontWeight = "bold"
+    job.style.textAlign = "center"
+}
+
+function authorStyle() {
+    author.textContent = item.name
+    author.style.color = "#333"
+    author.style.textAlign = "center"
+}
+
+function infoStyle() {
+    info.textContent = item.text
+    info.style.fontSize = "smaller"
+    info.style.letterSpacing = "1px"
+    info.style.color = "#333"
+}
