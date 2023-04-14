08c9b1782f4ed001ca1b66508791d16ae9c4d2ce
Jeric Viernes <jericviernes06@gmail.com>
2023-04-14 20:25:25 +0800

    added const reviews

---
+++ Reviews/app.js
@@ -0,0 +1,16 @@
+const reviews = [
+    {
+        id:1,
+        name:"Jeric James Viernes",
+        job:"Full stack developer",
+        img:"pic-2.jpg",
+        text:"Hello, my name is Jeric James Viernes. I'm 20 years old from Cabuyao Laguna. I'm currently at 2nd year college taking Bachelor of Science in Information Technology. I specialized in programming language such as Javascript and Python."
+    },
+    {
+        id:2,
+        name:"ChatGPT",
+        job:"AI Model",
+        img:"pic-3.jpg",
+        text:"Hello, my name is ChatGPT. I am a language model designed to assist and communicate with humans. My purpose is to provide helpful and informative responses to any questions or inquiries you may have."
+    }
+]
